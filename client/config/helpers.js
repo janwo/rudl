bb7ac5dc4ed75b403411eb8df16ed78d5f2c299f
Jan Wolf <mail@jan-wolf.de>
2016-09-07 14:37:35 +0200

    Remove ignored files

--- client/config/helpers.js
+++
@@ -1,6 +0,0 @@
-var Path = require( 'path' );
-var _root = Path.resolve( __dirname, '..' );
-exports.root = function ( args ) {
-	args = Array.prototype.slice.call( arguments, 0 );
-	return Path.join.apply( Path, [ _root ].concat( args ) );
-};
--- server/config/plugins/Authentification.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.PluginsConfig = [
-    { register: require('bell') },
-    { register: require('hapi-auth-jwt2') },
-    { register: require('hapi-auth-basic') }
-];
-//# sourceMappingURL=Authentification.js.map
