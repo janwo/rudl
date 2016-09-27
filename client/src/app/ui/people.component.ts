0c0e97bb58997c185749de243731ceb9de423e55
Jan Wolf <mail@jan-wolf.de>
2016-09-27 15:18:25 +0200

    Improved people template

--- client/src/app/ui/people.component.ts
+++ client/src/app/ui/people.component.ts
@@ -1,10 +1,19 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {UserService} from "../user.service";
 
 @Component({
     template: require('./people.component.html'),
     styles: [require('./people.component.scss')]
 })
-export class PeopleComponent {
-
-    constructor() {}
+export class PeopleComponent implements OnInit {
+    
+    followingUsers: any;
+    
+    constructor(
+        private userService: UserService
+    ) {}
+    
+    ngOnInit(){
+        
+    }
 }
