9c6b9cc1831394d65543e2f978cc35097c1f19c4
Jan Wolf <mail@jan-wolf.de>
2017-02-27 19:50:30 +0100

    Fixed Facebook button

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,10 +1,11 @@
-import {Component} from "@angular/core";
+import {Component, ViewEncapsulation} from "@angular/core";
 import {DataService} from "./services/data.service";
 import {Router} from "@angular/router";
 
 @Component({
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
+    encapsulation: ViewEncapsulation.Emulated,
     selector: 'app'
 })
 export class AppComponent {
