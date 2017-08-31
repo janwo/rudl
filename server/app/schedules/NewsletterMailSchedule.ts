c4fd2f5d3e33b3e6a759554de14b1d5195a6aa0e
Jan Wolf <mail@jan-wolf.de>
2017-08-31 16:51:48 +0200

    Make newsletter ready to use

--- server/app/schedules/NewsletterMailSchedule.ts
+++ server/app/schedules/NewsletterMailSchedule.ts
@@ -7,16 +7,17 @@ import {getBestLanguage, Locale} from "../models/Translations";
 
 export const ScheduleConfig = {
 	rule: {
-		second: 0
+		second: 0,
+		minute: 15,
+		hour: 7
 	},
 	job: () => {
 		// Send notification mails.
 		let session = DatabaseManager.neo4jClient.session();
-		return session.run(`MATCH(n:Newsletter {ready: true, sent: false}) SET n.sent = true WITH COLLECT(properties(n)) as newsletters MATCH(ns:Settings {newsletterMails: true})<-[:USER_SETTINGS]-(u:User) WHERE u.username = 'maziar' OR u.username = 'jntaylor' WITH COLLECT(properties(u)) as users, newsletters RETURN {users: users, newsletters: newsletters} as result`).then((result: any) => DatabaseManager.neo4jFunctions.unflatten(result.records, 'result').shift()).then((result: {
+		return session.run(`MATCH(n:Newsletter {ready: true, sent: false}) SET n.sent = true WITH COLLECT(properties(n)) as newsletters MATCH(ns:Settings {newsletterMails: true})<-[:USER_SETTINGS]-(u:User) WITH COLLECT(properties(u)) as users, newsletters RETURN {users: users, newsletters: newsletters} as result`).then((result: any) => DatabaseManager.neo4jFunctions.unflatten(result.records, 'result').shift()).then((result: {
 			users: User[],
 			newsletters: Newsletter[]
         }) => {
-		    console.log(result);
 		    let mailJobs: NewsletterMailOptions[] = [];
             result.users.forEach(user => {
                 result.newsletters.forEach(newsletter => {
